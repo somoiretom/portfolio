655e4b3b83f6a758c139bb52397c02c59b9de8f7
somoiretom <85219948+somoiretom@users.noreply.github.com>
2021-10-22 17:06:14 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 function Calculator(e) {
   let num1 = document.getElementById("num1");
-  let num1 = document.getElementById("num1");
+  let num2 = document.getElementById("num2");
+  let output = document.getElementById("output");
 
   num1 = parseInt(num1.value);
   num2 = parseInt(num2.value);
@@ -9,7 +10,15 @@ function Calculator(e) {
   const diff = num1 - num2;
   const product = num1 * num2;
   const quot = num1 / num2;
-
+  output.innerHTML =
+    "Sum: " +
+    sum +
+    "\nDifference: " +
+    diff +
+    "\nProduct: " +
+    product +
+    "\nquotient: " +
+    quot;
   //console.log("Sum :" + sum);
   //console.log("Difference :" + diff);
   //console.log("Product :" + product);
@@ -18,4 +27,9 @@ function Calculator(e) {
   console.log("Type of quotient:" + typeof quot);
 }
 
-Calculator(200, 56);
+let navbar_icon = document.getElementById("navbar-icon");
+navbar_icon.addEventListener("click", function () {
+  let collapse = document.getElementById("collapse");
+  if (collapse.style.display == "none") collapse.style.display = "flex";
+  else collapse.style.display = "nonw";
+});
